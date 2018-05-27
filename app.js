31d9d7c902213fddef585cf828d8535eab9fa550
RavAlmasy <eb.vega.m@gmail.com>
2018-05-27 13:17:17 -0400

    Resuelve problema, pero no hace nada extra

---
+++ app.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+ return num1 + num2;
+};
+
+function multiply(a, b) {
+  var b = add(9824, 777);
+  return a * b;
+};
+
+document.write('36325 x (9824 + 777) = ' + multiply(36325, add))
+
+console.log(multiply(36325, add));
+
+
+
+
+
+
